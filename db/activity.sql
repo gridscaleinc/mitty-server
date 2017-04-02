fc856cf171b2ab1a5a7843857333d6b984a44906
domanthan <domanthan@gmail.com>
2017-04-02 22:27:37 +0900

    SQL files

---
+++ db/activity.sql
@@ -0,0 +1,13 @@
+create table activity (
+        id	        	serial8	,
+        seq			int2	,
+        eventId			int8	,
+        subscribtionType	varchar	(20),
+        notification		boolean	,
+        restaurantSuggestOk	boolean	,
+        hotelSuggestOk		boolean	,
+        movingSuggestOk		boolean	,
+        url			varchar	(100),
+        memo			text	,
+        PRIMARY KEY(id,seq)
+);
---
+++ db/vehicle.sql
@@ -0,0 +1,9 @@
+create table vehicle(
+    islandId              int4 PRIMARY KEY,
+    serviceNo             varchar(64),
+    category              varchar(20),
+    tonage                int2,
+    numberOfSeats         int2,
+    apartPlace            varchar(100),
+    terminalPlace         varchar(100)
+);
