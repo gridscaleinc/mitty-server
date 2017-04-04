1bec430201905f80599f9e937d145f102f13d4c1
Dongri Jin <dongrify@gmail.com>
2017-04-05 07:32:50 +0900

    Add: migration files

---
+++ db/migrations/20170405070924_AddUser.sql
@@ -0,0 +1,20 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+create table if not exists users (
+  id              serial PRIMARY KEY,
+  name			      varchar(30),
+  user_name		    varchar(30),
+  password		    varchar(255),
+  access_token		varchar(255),
+  mail_address		varchar(50),
+  mail_cnfirmed		boolean,
+  status		      varchar	(20),
+  created		      timestamp  not null DEFAULT CURRENT_TIMESTAMP,
+  updated		      timestamp  not null DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE          (user_name)
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+drop table users cascade;
