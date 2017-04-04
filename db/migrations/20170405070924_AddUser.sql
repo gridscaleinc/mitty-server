2b343e7a745633e826a809f162c9c9efcfd7616f
Dongri Jin <dongrify@gmail.com>
2017-04-05 07:37:57 +0900

    Typo

--- db/migrations/20170405070924_AddUser.sql
+++ db/migrations/20170405070924_AddUser.sql
@@ -3,15 +3,15 @@
 -- SQL in section 'Up' is executed when this migration is applied
 create table if not exists users (
   id              serial PRIMARY KEY,
-  name			      varchar(30),
-  user_name		    varchar(30),
-  password		    varchar(255),
-  access_token		varchar(255),
-  mail_address		varchar(50),
-  mail_cnfirmed		boolean,
-  status		      varchar	(20),
-  created		      timestamp  not null DEFAULT CURRENT_TIMESTAMP,
-  updated		      timestamp  not null DEFAULT CURRENT_TIMESTAMP,
+  name            varchar(30),
+  user_name       varchar(30),
+  password        varchar(255),
+  access_token    varchar(255),
+  mail_address    varchar(50),
+  mail_confirmed  boolean,
+  status          varchar	(20),
+  created         timestamp  not null DEFAULT CURRENT_TIMESTAMP,
+  updated         timestamp  not null DEFAULT CURRENT_TIMESTAMP,
   UNIQUE          (user_name)
 );
 
