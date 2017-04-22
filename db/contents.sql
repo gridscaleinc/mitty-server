52fa2648aeb19fdf5eeb45d6c825968f54dbc265
domanthan <domanthan@gmail.com>
2017-04-22 10:03:32 +0900

    項目名をunderline区切りに戻す。やっばり。 ContentsデータはS3に入れる前提に変更。

--- db/contents.sql
+++ db/contents.sql
@@ -2,11 +2,11 @@ drop table contents cascade;
 create table contents (
 	id		serial8	PRIMARY KEY,		
 	mime		varchar	(50),		
-	title		varchar	(100),		
-	linkUrl		varchar	(100),		
+	name		varchar	(100),		
+	link_url		varchar	(1000),		
 	width		int2	,		
 	height		int2	,		
-	data		bytea	,		
 	size		int	,		
-	lastUpdated	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+	created    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
--- db/gallery.sql
+++ db/gallery.sql
@@ -3,8 +3,7 @@ create table gallery (
 	id		serial4	PRIMARY KEY,
 	seq		int2,
 	caption		varchar	(100),
-	briefInfo	text,
-	url		varchar	(100),
-	contentId	int8,
-	freeText	text
+	brief_info	text,
+	content_id	int8,
+	free_text	text
 )
