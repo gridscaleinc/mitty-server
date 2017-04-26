c268214aa0e823dfea973d38ba718a1c61d4223d
domanthan <domanthan@gmail.com>
2017-04-26 22:51:29 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	db/gallery.sql

--- db/gallery.sql
+++ db/gallery.sql
@@ -1,11 +1,12 @@
 drop table gallery cascade ;
 create table gallery (
-	id		serial4	PRIMARY KEY,
+	id		serial4	,
 	seq		int2,
 	caption		varchar	(100),
 	brief_info	text,
 	content_id	int8,
 	free_text	text,
 	created    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+	updated	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id,seq)
 )
