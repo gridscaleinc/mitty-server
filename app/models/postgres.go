0e8d90510d2d309bb96b5b3497b3a632e50ce5cd
Dongri Jin <dongrify@gmail.com>
2017-05-02 13:10:03 +0900

    Add: table

--- app/models/postgres.go
+++ app/models/postgres.go
@@ -9,4 +9,6 @@ func AddTableWithName(dbmap *gorp.DbMap) {
 	dbmap.AddTableWithName(Gallery{}, "gallery").SetKeys(true, "ID")
 	dbmap.AddTableWithName(Island{}, "island").SetKeys(true, "ID")
 	dbmap.AddTableWithName(Contents{}, "contents").SetKeys(true, "ID")
+	dbmap.AddTableWithName(Activity{}, "activity").SetKeys(true, "ID")
+	dbmap.AddTableWithName(ActivityItem{}, "activity_item").SetKeys(true, "ID")
 }
