c687d06c0998a09fea7f72442ac3604a3985b756
Dongri Jin <dongrify@gmail.com>
2017-05-05 00:34:20 +0900

    Fixz

--- db/migrations/20170502105359_AddActivityItem.sql
+++ db/migrations/20170502105359_AddActivityItem.sql
@@ -2,6 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 create table activity_item (
+  id	        	          serial8	NOT NULL PRIMARY KEY,
   activity_id            int8,
   event_id	             int8,
   title	                 varchar(200),
@@ -9,8 +10,7 @@ create table activity_item (
   notification           bool,
   notificationDateTime   date,
   created                timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated	               timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY(activity_id, event_id)
+  updated	               timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
