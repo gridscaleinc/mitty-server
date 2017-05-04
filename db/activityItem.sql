be329ced7b13a7bd3d94b6a7ce8edef44b0ff958
Dongri Jin <dongrify@gmail.com>
2017-05-05 00:24:52 +0900

    Fix

--- db/activityItem.sql
+++ db/activityItem.sql
@@ -1,12 +1,13 @@
 drop table activity_item cascade;
 create table activity_item (
-        activity_id                       int8 ,	
-        event_id	                       int8	,
-        title	                           varchar(200),
-        memo		                   text,	
-        notification                  bool,	
-        notificationDateTime   date,	
-        created    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        id	        	                    serial8	NOT NULL PRIMARY KEY,
+        activity_id                       int8 ,
+        event_id	                        int8	,
+        title	                            varchar(200),
+        memo		                          text,
+        notification                      bool,
+        notificationDateTime              date,
+        created                           timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated	                          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY(activity_id, event_id)
 )
