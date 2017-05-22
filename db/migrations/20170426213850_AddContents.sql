c9295b2ffa4196fc59021805fcf9857e34a34ace
Dongri Jin <dongrify@gmail.com>
2017-05-22 13:56:29 +0900

    Fix: sql

--- db/migrations/20170426213850_AddContents.sql
+++ db/migrations/20170426213850_AddContents.sql
@@ -2,16 +2,17 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 create table if not exists contents (
-  id		      serial8	PRIMARY KEY,
-	mime		    varchar	(50),
-	name		    varchar	(100),
-	link_url		varchar	(1000),
-	width		    int2,
-	height		  int2,
-	size		    int,
-  owner_id    int,
-	created     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated	    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  id		        serial8	PRIMARY KEY,
+	mime		      varchar	(50),
+	name		      varchar	(100),
+	link_url		  varchar	(1000),
+  thumbnail_url varchar	(1000),
+	width		      int2,
+	height		    int2,
+	size		      int,
+  owner_id      int,
+	created       timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated	      timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
