d18bab1dca410a220cb014938f2acde1597538e5
domanthan <domanthan@gmail.com>
2017-06-04 18:30:19 +0900

    イベント会議一覧

---
+++ app/controllers/meeting.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"net/http"
+
+	"mitty.co/mitty-server/app/filters"
+	"mitty.co/mitty-server/app/helpers"
+	"mitty.co/mitty-server/app/models"
+
+	"github.com/mholt/binding"
+)
+
+// FetchingConversation ...
+func GetEventMeeting(w http.ResponseWriter, r *http.Request) {
+	render := filters.GetRenderer(r)
+	dbmap := helpers.GetPostgres()
+	tx, err := dbmap.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+    userID := 0
+	meetingList, err := models.GetEventMeetingList(tx,userID)
+	if err != nil {
+		filters.RenderError(w, r, err)
+		return
+	}
+	render.JSON(w, http.StatusOk, map[string]interface{}{
+		"eventMeetingList": meetingList,
+	})
+}
--- app/models/meeting.go
+++ app/models/meeting.go
@@ -29,3 +29,32 @@ func (s *Meeting) Update(tx gorp.Transaction) error {
 	_, err := tx.Update(s)
 	return err
 }
+type EventMeeting struct {
+	ID      int64     `db:"id" json:"id"`
+	Name    string    `db:"name" json:"name"`
+	Type    string    `db:"type" json:"type"`
+	EventTitle string `db:"event_title" json:"eventTitle"`
+	EventLogoUrl string `db:"event_logo_url" json:"eventLogoUrl"`
+	Created time.Time `db:"created" json:"created"`
+	Updated time.Time `db:"updated" json:"updated"`
+}
+
+// Get Latest Conversation ...
+func GetEventMeetingList(tx *gorp.Transaction,  userID int64) ([]EventMeeting, error) {
+	eventMeeting := []EventMeeting{}
+	_, err := tx.Select(&eventMeeting, `
+	select 
+	    meeting.*,
+	    events.title as event_title,
+	    contents.link_url as event_logo_url 
+	from 
+	    meeting inner join events on events.meeting_id=meeting.id 
+	    inner join activity_item on activity_item.event_id=events.id 
+	    inner join activity on activity.id=activity_item.activity_id 
+	    left outer join contents on contents.id=events.logo_id 
+	where 
+	   activity.owner_id=$1 
+	order by 
+	   events.start_datetime, events.end_datetime;`, userID)
+	return eventMeeting, err
+}
