0f7d90a392ad358ca7d167072521f7e0c177b52b
domanthan <domanthan@gmail.com>
2017-08-01 22:53:17 +0900

    Duplicated

--- app/models/namecard.go
+++ app/models/namecard.go
@@ -8,22 +8,20 @@ import (
 
 // Likes struct
 type Namecard struct {
-	ID              int64  `db:"id" json:"id"`
-	MittyID         int    `db:"mitty_id" json:"mitty_id"`
-	BusinessNae     string `db:"business_name" json:"business_name"`
-	BusinessSubName string `db:"business_sub_name" json:"business_sub_name"`
-	BusinessTitle   string `db:"business_title" json:"business_title"`
-	AddressLine1    string `db:"address_line1" json:"address_line1"`
-	AddressLine2    string `db:"address_line2" json:"address_line2"`
-
+	ID              int64     `db:"id" json:"id"`
+	MittyID         int       `db:"mitty_id" json:"mitty_id"`
+	BusinessNae     string    `db:"business_name" json:"business_name"`
 	BusinessSubName string    `db:"business_sub_name" json:"business_sub_name"`
+	BusinessTitle   string    `db:"business_title" json:"business_title"`
+	AddressLine1    string    `db:"address_line1" json:"address_line1"`
+	AddressLine2    string    `db:"address_line2" json:"address_line2"`
 	Phone           string    `db:"phone" json:"phone"`
 	Fax             string    `db:"fax" json:"fax"`
 	MobilePhone     string    `db:"mobile_phone" json:"mobile_phone"`
 	Webpage         string    `db:"webpage" json:"webpage"`
 	Email           string    `db:"email" json:"email"`
 	Created         time.Time `db:"created" json:"created"`
-	Update          time.Time `db:"updated" json:"updated"`
+	Updated         time.Time `db:"updated" json:"updated"`
 }
 
 // Insert ...
