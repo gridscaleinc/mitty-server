afc206d1e6cc75747d290bcec44a921ef1ebeb22
domanthan <domanthan@gmail.com>
2017-08-21 23:28:16 +0900

    url

--- app/routers.go
+++ app/routers.go
@@ -60,6 +60,7 @@ func publicRoutes(r *mux.Router) {
 	r.Handle("/new/activity/item", apiAuth(controllers.PostActivityItemHandler)).Methods("POST")
 	r.Handle("/new/island", apiAuth(controllers.PostIslandHandler)).Methods("POST")
 	r.Handle("/new/request", apiAuth(controllers.PostRequestHandler)).Methods("POST")
+	r.Handle("/save/namecard", apiAuth(controllers.PostNameCardHandler)).Methods("POST")
 	r.Handle("/save/profile", apiAuth(controllers.PostProfileHandler)).Methods("POST")
 	r.Handle("/new/proposal", apiAuth(controllers.PostProposalHandler)).Methods("POST")
 	r.Handle("/gallery/content", apiAuth(controllers.PostGalleryContentHandler)).Methods("POST")
