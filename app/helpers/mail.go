f6f18c03182d35650272f68e01844feb83b261a7
Dongri Jin <dongrify@gmail.com>
2017-09-09 06:09:28 +0900

    Amazon SES

--- app/helpers/mail.go
+++ app/helpers/mail.go
@@ -1,26 +1,55 @@
 package helpers
 
 import (
-	"fmt"
+	"errors"
+	"log"
 
-	"gopkg.in/mailgun/mailgun-go.v1"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 // SendEmail ...
-func SendEmail(from, to, subject, text string) error {
-	domain := "mitty.co"
-	apiKey := "key-04584e6b040b239e20234768c1209c77"
-	publicAPIKey := "pubkey-4918c3ad10a37886f15a84d0def08f86"
-	mg := mailgun.NewMailgun(domain, apiKey, publicAPIKey)
-	message := mailgun.NewMessage(
-		from,
-		subject,
-		text,
-		to)
-	resp, id, err := mg.Send(message)
+func SendEmail(from, to, title, body string) error {
+	err := sendSESEmail(from, to, title, body)
 	if err != nil {
+		log.Println("mail sending error")
 		return err
 	}
-	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+	return nil
+}
+
+func sendSESEmail(from string, to string, title string, body string) error {
+	awsSession := session.New(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials("AKIAI6WJQ2KNFSEAB4OQ", "61XGKqSGs6VcEDvOONaqp6zWbaINH1GEbTDw4fXI", ""),
+	})
+
+	svc := ses.New(awsSession)
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: []*string{
+				aws.String(to),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Text: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String(body),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
+				Data:    aws.String(title),
+			},
+		},
+		Source: aws.String(from),
+	}
+	_, err := svc.SendEmail(input)
+	if err != nil {
+		return errors.New(err.Error())
+	}
 	return nil
 }
